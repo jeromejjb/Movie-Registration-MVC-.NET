8531a6a3460940a34ef4ded0e32d0de6badfe157
jerome <jeromebrown97@gmail.com>
2021-06-29 22:14:48 -0500

    inital commit for MVC NET Movie Registration

---
+++ MovieRegustration/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegustration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegustration.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+      MovieDbContext movieDb = new MovieDbContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RegistrationForm()
+        {
+
+            return View();
+        }
+        public IActionResult Result(Movie inputMovie)
+        {
+            
+            return View(inputMovie);
+            
+
+        }
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieRegustration/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegustration.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public int Runtime { get; set; }
+
+    
+    }
+}
